f528d0e29655fe12422b91f6ab1748f265bab6bc
alexdinh2003 <101940926+alexdinh2003@users.noreply.github.com>
2023-01-02 23:05:09 -0500

    Add files via upload

--- StudentManagement/MainPage.java
+++ StudentManagement/MainPage.java
@@ -5,8 +5,10 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.text.NumberFormat;
 
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -18,6 +20,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.text.NumberFormatter;
 
 
 public class MainPage implements ActionListener {
@@ -25,7 +28,7 @@ public class MainPage implements ActionListener {
     final static Font mainFont2 = new Font("Tahoma", Font.PLAIN, 19);
     Object[] row;
 
-    JLabel lblFirstName, lblLastName, lblClass, lblFinalGrade;
+    JLabel lblFirstName, lblLastName, lblClass, lblFinalGrade, lblStudentID;
     JButton btnAdd, btnDelete;
     JMenuBar menuBar;
     JMenu fileMenu, editMenu, helpMenu;
@@ -37,7 +40,8 @@ public class MainPage implements ActionListener {
     //final private Font mainFront = new Font("Segoe print", Font.BOLD, 20);
     //private static final String Font = null;
 
-    private static JTextField textStudentID, textFirstName, textLastName, textGrade, textFinalGrade;
+    private static JTextField textStudentID, textFirstName, textLastName;
+    JFormattedTextField textGrade, textFinalGrade;
     public static void main(String[] args){
         //MangagePage();
     }
@@ -47,6 +51,7 @@ public class MainPage implements ActionListener {
         frame = new JFrame("Student Management");
         frame.setBounds(100,100,467,315);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
         frame.getContentPane().setLayout(null);
 
         menuBar = new JMenuBar();
@@ -89,11 +94,11 @@ public class MainPage implements ActionListener {
 
         //Create a table 
         table = new JTable();
-        Object[] cols = {"Student ID", "First Name", "Last Name", "Class", "Final Grade"};
+        Object[] cols = {"First Name", "Last Name", "Class", "Final Grade", "Student ID"};
         DefaultTableModel model = new DefaultTableModel();
         frame.getContentPane().setBackground(new Color(0, 0, 0));                
         frame.getContentPane().setForeground(Color.WHITE);
-        frame.setBounds(100, 100, 630, 610);//720
+        frame.setBounds(100, 100, 630, 660);//720
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
         frame.setLocationRelativeTo(null);
@@ -118,32 +123,33 @@ public class MainPage implements ActionListener {
         frame.getContentPane().add(pane);
 
         // Student ID, Text First, Last Name, grade = class, final grade
-        textStudentID = new JTextField();
-        textStudentID.setBounds(121, 374, 184, 35);
-        textStudentID.setBackground(Color.WHITE);
-        frame.getContentPane().add(textStudentID);
-        textStudentID.setColumns(10);
-
         textFirstName = new JTextField();
         textFirstName.setBounds(121, 374, 184, 35);
         textFirstName.setBackground(Color.WHITE);
         frame.getContentPane().add(textFirstName);
         textFirstName.setColumns(10);
 
+        // (121, 418, 184, 35);
         textLastName = new JTextField();
-        textLastName.setBounds(121, 418, 184, 35);
+        textLastName.setBounds(432, 374, 171, 35);
         frame.getContentPane().add(textLastName);
         textLastName.setColumns(10);
 
-        textGrade = new JTextField();
-        textGrade.setBounds(432, 374, 171, 35);
+        textGrade = new JFormattedTextField(NumberFormat.getNumberInstance());
+        textGrade.setBounds(121, 418, 184, 35);
         frame.getContentPane().add(textGrade);
         textGrade.setColumns(10);
 
-        textFinalGrade = new JTextField();
+        textFinalGrade = new JFormattedTextField(NumberFormat.getNumberInstance());
         textFinalGrade.setBounds(432, 418, 171, 35);
         frame.getContentPane().add(textFinalGrade);
         textFinalGrade.setColumns(10);
+
+        textStudentID = new JTextField();
+        textStudentID.setBounds(121, 462, 184, 35);
+        textStudentID.setBackground(Color.WHITE);
+        frame.getContentPane().add(textStudentID);
+        textStudentID.setColumns(10);
 
         //DO Label
         lblFirstName = new JLabel("First Name");
@@ -170,12 +176,19 @@ public class MainPage implements ActionListener {
         lblFinalGrade.setForeground(Color.WHITE);
         lblFinalGrade.setBounds(326, 429, 103, 20);
         frame.getContentPane().add(lblFinalGrade);
+
+        lblStudentID = new JLabel("Student ID");
+        lblStudentID.setFont(mainFont2);
+        lblStudentID.setForeground(Color.WHITE);
+        lblStudentID.setBounds(10, 473, 103, 20);
+        frame.getContentPane().add(lblStudentID);
         // ROW
-        row = new Object[4];
+        row = new Object[5];
+
 
         //Buttons ADD
         btnAdd = new JButton("ADD");
-        btnAdd.setBounds(10, 463, 295, 35);
+        btnAdd.setBounds(10, 507, 295, 35);
         frame.getContentPane().add(btnAdd);
         btnAdd.addActionListener(new ActionListener(){
 
@@ -184,33 +197,62 @@ public class MainPage implements ActionListener {
 
                 MainDepartment department = new MainDepartment();
                 
+                //Validate input data
+                if(textFirstName.getText().isEmpty() || textLastName.getText().isEmpty() || textGrade.getText().isEmpty() || textFinalGrade.getText().isEmpty() || textStudentID.getText().isEmpty()){
+                    JOptionPane.showMessageDialog(frame, "All information required!");
+                    return;
+                }
+                if(department.IsExistingStudentID(textStudentID.getText().trim())){
+                    JOptionPane.showMessageDialog(frame, "Duplicated student ID!");
+                    return;
+                }
 
                 row[0] = textFirstName.getText();
                 row[1] = textLastName.getText();
                 row[2] = textGrade.getText().trim();
                 row[3] = textFinalGrade.getText().trim();
+                row[4] = textStudentID.getText().trim();
 
-                if (department.insertGradeKeeper((String) row[0], (String) row[1], Integer.parseInt((String)row[2]), Integer.parseInt((String)row[3]))) {
+                try{
+                if (department.insertGradeKeeper((String) row[0], (String) row[1], Integer.parseInt((String)row[2]), Integer.parseInt((String)row[3]), (String) row[4])) {
                     model.addRow(row);
+                    JOptionPane.showMessageDialog(frame, "Inserted Sucessfully!");
                 }else{
                     //JOptionPane.showMessageDialog("error");
 
                     System.out.println("error");
+                }
+                }catch (Exception e){
+                    JOptionPane.showMessageDialog(frame, e.getMessage());
+
+                    
+
                 }
             }
         });
 
         //Buttons DELETE
         btnDelete = new JButton("DELETE");
-        btnDelete.setBounds(313, 463, 290, 35);
+        btnDelete.setBounds(313, 507, 290, 35);
         frame.getContentPane().add(btnDelete);
         btnDelete.addActionListener(new ActionListener(){
 
             @Override
             public void actionPerformed(ActionEvent arg0){
+                MainDepartment department = new MainDepartment();
+
                 int i = table.getSelectedRow();
                 if(i >= 0){
-                    model.removeRow(i);
+                    System.out.println(i);
+                    System.out.println(table.getValueAt(i, 4));
+                    if(department.deleteGradeKeeper((String) table.getValueAt(i, 4))){
+                        model.removeRow(i);
+                        JOptionPane.showMessageDialog(frame, "Deleted Sucessfully!");
+                    }else{
+                        JOptionPane.showMessageDialog(frame, "Deleted Error!");                       
+
+                    }
+                    
                 }else{
                     JOptionPane.showMessageDialog(frame, "Delete Error!");
                 }
@@ -235,7 +277,7 @@ public class MainPage implements ActionListener {
         }
         if(e.getSource() == exitItem){
             frame.setVisible(false);
-            GUI_Department back = new GUI_Department();
+            Login back = new Login();
             back.welcomePage();
             back.UserLogin();
         }
